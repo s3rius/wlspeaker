53b5eaeba6f334a49100f90d0400c4ca1b3adb3a
Pavel Kirilin <win10@list.ru>
2021-01-26 13:36:10 +0400

    Added rate param.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- wlspeaker/client.py
+++ wlspeaker/client.py
@@ -1,3 +1,5 @@
+import argparse
+
 import pulsectl
 from loguru import logger
 
@@ -7,6 +9,9 @@ from wlspeaker.resolver import find_network_neighbours
 
 def main():
     """Find all available speakers and enable them."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--rate", type=int, default=1411)  # noqa: WPS432
+    script_args = parser.parse_args()
     configure_logging()
     neighbours_list = find_network_neighbours()
     with pulsectl.Pulse("wlspeaker-client") as pulse:
@@ -15,7 +20,7 @@ def main():
             pulse.module_load(
                 "module-tunnel-sink-new",
                 "channels=2 "
-                "rate=48000 "
+                f"rate={script_args.rate} "
                 f"server={neighbour} "
                 f"sink_name=wl_speaker_{neighbour} "
                 f"sink_properties=device.description=wl_{neighbour}",
